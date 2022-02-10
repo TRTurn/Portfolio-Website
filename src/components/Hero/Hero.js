a5fc007deeb7e4e2378708971f4cdcfaf454a938
Tim Turnbaugh <turnbaugh.tim@gmail.com>
2022-02-10 11:11:22 -0800

    Update hero section title and section text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,10 +8,10 @@ const Hero = (props) => (
 	<Section row nopadding>
 		<LeftSection>
 			<SectionTitle main center>
-				Welcome To <br />
+				Welcome to <br />
 				My Portfolio
 			</SectionTitle>
-			<SectionText>My goal is to create novel web products using the latest technologies available.</SectionText>
+			<SectionText>My goal is to create novel applications and web products using the latest technologies available.</SectionText>
 			<Button onClick={() => window.open("https://github.com/TRTurn","_blank")}>See My Code</Button>
 		</LeftSection>
 	</Section>
